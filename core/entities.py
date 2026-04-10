dd1a153d25333efb545ce6129c096dd9b52b54d2
Mayene Doria <mayene.contato@gmail.com>
2026-04-10 09:46:24 -0300

    feat: entities finalizado com type hints

--- core/entities.py
+++ core/entities.py
@@ -4,39 +4,39 @@ from dataclasses import dataclass
 
 
 RISK_LABELS = [
-    "Sem Capacete",
-    "Sem Colete",
-    "Sem Oculos",
+    "sem_capacete",
+    "sem_colete",
+    "sem_oculos",
 ]
 
 @dataclass
 class Detection:
     label: str
     confidence: float
-    x1: int
-    y1: int
-    x2: int
-    y2: int
+    x1: float
+    y1: float
+    x2: float
+    y2: float
     
        
     @property
-    def center_x(self):
+    def center_x(self) -> float:
         return (self.x1 + self.x2) / 2
 
     @property
-    def center_y(self):
+    def center_y(self) -> float:
         return (self.y1 + self.y2) / 2
     
     @property
-    def width(self):
+    def width(self) -> float:
         return (self.x2 - self.x1)
     
     @property
-    def height(self):
+    def height(self) -> float:
         return (self.y2 - self.y1)
     
     @property
-    def is_risk(self):
+    def is_risk(self) -> bool:
         return self.label in RISK_LABELS
 
 @dataclass
@@ -47,9 +47,9 @@ class IncidentEntry:
     img_path: str
         
     @property
-    def img_name(self):
+    def img_name(self) -> str:
         return os.path.basename(self.img_path)
     
     @property
-    def time_short(self):
+    def time_short(self) -> str:
         return self.timestamp.strftime("%H:%M:%S")
