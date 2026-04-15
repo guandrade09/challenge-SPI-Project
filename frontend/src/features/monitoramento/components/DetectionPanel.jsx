cc8209bc2610521cec4aa39baf12d922b426ebda
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-15 20:42:42 -0300

    chore: adicionando componente DetectionPanel

---
+++ frontend/src/features/monitoramento/components/DetectionPanel.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { DetectionCard } from './DetectionCard';
+import { useMonitoramentoStore } from '../store/useMonitoramentoStore';
+
+export const DetectionPanel = ({ options }) => {
+  // Pegamos o estado e a função de disparar o toggle da nossa Store (Zustand)
+  const { detections, toggleDetection } = useMonitoramentoStore();
+
+  return (
+    <div className="flex flex-col items-end w-full">
+      {options.map((option) => (
+        <DetectionCard
+          key={option.id}
+          label={option.label}
+          isChecked={!!detections[option.id]} // Força booleano (default false)
+          onToggle={() => toggleDetection(option.id)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default DetectionPanel;
