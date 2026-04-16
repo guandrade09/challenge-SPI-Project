ff8bdff77ca81d70574b878fad6807ac50a71788
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-16 20:07:30 -0300

    refactor: breafing do navbar

--- frontend/src/layouts/NavBar.jsx
+++ frontend/src/layouts/NavBar.jsx
@@ -12,21 +12,24 @@ const navItems = [
 
 export const NavBar = () => {
   return (
-    <nav className="flex justify-center items-center gap-10 py-6 mb-8">
+    <nav className="flex justify-center items-center w-full">
+      <div className='flex items-center'>
       {navItems.map((item) => (
         <button
           key={item.id}
           title={item.label}
           className={cn(
-            "transition-all duration-300 ease-in-out hover:scale-110",
+            "transition-all duration-300 border-b-2 pb-1",
+            "mx-20",
             item.active 
-              ? "text-white border-b-2 border-white pb-1" 
-              : "text-zinc-500 hover:text-zinc-300"
+              ? "text-white border-white" 
+              : "text-zinc-500 border-transparent hover:text-zinc-300"
           )}
         >
           <item.icon size={28} strokeWidth={item.active ? 2.5 : 2} />
         </button>
       ))}
+      </div>
     </nav>
   );
 };
