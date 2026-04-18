7d474ea5c48d81f2a3598a217f85c4cf70072e32
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-18 06:49:02 -0300

    chore: criando um exportador __cleancode__

---
+++ frontend/src/features/logsPage/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as DashboardChart } from './DashboardChart';
+export { default as DetectionBarChart } from './DetectionBarChart';
+export { default as DetectionLineChart } from './DetectionLineChart';
+export { default as LogPanel } from './LogPanel';
+export { default as MessageConsole } from './MessageConsole';
