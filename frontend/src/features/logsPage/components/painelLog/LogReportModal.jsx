ca3f9c374208d782536188976ab20e0dbf3ad72b
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-20 16:22:46 -0300

    refactor: arrumando popup gerado na tela de logs

--- frontend/src/features/logsPage/components/painelLog/LogReportModal.jsx
+++ frontend/src/features/logsPage/components/painelLog/LogReportModal.jsx
@@ -4,6 +4,8 @@ import { FileText, CheckCircle2 } from 'lucide-react';
 import { PopupModal } from '../../../../components/shared/PopupModal';
 
 export const LogReportModal = ({ isOpen, onClose, data }) => {
+  if (!data) return null;
+
   return (
     <PopupModal 
       isOpen={isOpen} 
@@ -14,19 +16,20 @@ export const LogReportModal = ({ isOpen, onClose, data }) => {
       <div className="flex flex-col items-center text-center">
         <CheckCircle2 size={48} className="text-green-600 mb-4 animate-bounce" />
         
-        <span className="text-[10px] font-bold text-zinc-400 uppercase mb-1">
+        <span className="text-[10px] font-bold text-black uppercase mb-1">
           Status: {data.status}
         </span>
         
-        <p className="text-zinc-700 text-sm leading-relaxed mb-6">
-          {data.resumo}
-        </p>
-
-        <div className="w-full p-4 bg-white/50 rounded-xl border border-zinc-200 mb-6 text-left">
-          <p className="text-[10px] text-zinc-500 uppercase font-mono">
-            Gerado em: {data.data_geracao}
+        {/* CONTAINER DO SCROLL: Ajustado com max-h e custom-scrollbar */}
+        <div className="w-full max-h-[300px] bg-white/50 rounded-xl border border-zinc-200 mb-6 text-left overflow-y-auto custom-scrollbar">
+          <p className="text-[12px] text-zinc-800 font-medium p-4 leading-relaxed whitespace-pre-wrap">
+            {data.resumo}
           </p>
         </div>
+
+        <p className='text-[10px] text-black mb-4 font-mono'>
+          Gerado em: {data.data_geracao}
+        </p>
 
         <button 
           disabled 
