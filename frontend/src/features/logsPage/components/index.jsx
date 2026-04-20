c548c1273c47a6fb8e4a3b7cab2dd9936e409c45
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-20 02:59:24 -0300

    refactor: ajustes no exports

--- frontend/src/features/logsPage/components/index.jsx
+++ frontend/src/features/logsPage/components/index.jsx
@@ -1,5 +1,3 @@
-export { default as DashboardChart } from './DashboardChart';
-export { default as DetectionBarChart } from './DetectionBarChart';
-export { default as DetectionLineChart } from './DetectionLineChart';
 export { default as LogPanel } from './LogPanel';
 export { default as MessageConsole } from './MessageConsole';
+export { default as LogSettingsButton } from './LogSettingsButton'
