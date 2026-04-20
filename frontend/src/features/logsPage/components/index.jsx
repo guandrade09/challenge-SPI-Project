6a38fe5305f24c6b0c27d60ca1be48c83f6dfb2c
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-20 03:05:41 -0300

    refactor: ajustes nos exports

--- frontend/src/features/logsPage/components/index.jsx
+++ frontend/src/features/logsPage/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as LogPanel } from './LogPanel';
 export { default as MessageConsole } from './MessageConsole';
-export { default as LogSettingsButton } from './LogSettingsButton'
+export { default as LogSettingsButton } from './LogSettingsButton';
+export { default as LogSkeleton } from './LogSkeleton';
