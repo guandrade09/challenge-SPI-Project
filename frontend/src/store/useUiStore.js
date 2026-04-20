9c013987f11a0d7abca2012d5e07ece2026298cb
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-04-20 00:56:07 -0300

    chore: criando store de Ui para sideBar

---
+++ frontend/src/store/useUiStore.js
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+export const useUiStore = create((set) => ({
+  // Estado para a Sidebar de Configurações (Relatório)
+  isSidebarOpen: false,
+  toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+  closeSidebar: () => set({ isSidebarOpen: false }),
+
+  // Estado para a Sidebar da IA (O que o seu botão chama)
+  isAiSidebarOpen: false, 
+  toggleAiSidebar: () => set((state) => ({ isAiSidebarOpen: !state.isAiSidebarOpen })),
+  closeAiSidebar: () => set({ isAiSidebarOpen: false }),
+}));
