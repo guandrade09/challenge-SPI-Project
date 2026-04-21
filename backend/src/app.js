c1bca98a1630a7171942ac451f0e7a051e99bcd5
Lusca04 <rodriguwalusca@gmail.com>
2026-04-20 21:21:22 -0300

    fix: Updating app.js to confidence use endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import detectionRoutes from "./routes/detection.routes.js";
+import confidenceRoutes from "./routes/confidenceLog.routes.js";
 import { initDatabase } from "./config/database.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", detectionRoutes);
+app.use("/api", confidenceRoutes);
 
 async function startServer() {
   await initDatabase();
