01d7e636f4577fbfc4e9274a0d701edcccaca01d
Lusca04 <rodriguwalusca@gmail.com>
2026-04-21 22:03:55 -0300

    fix: Increase post detection payload limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import confidenceRoutes from "./routes/confidence.routes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use("/api", detectionRoutes);
 app.use("/api", confidenceRoutes);
 
