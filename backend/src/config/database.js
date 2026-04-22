7c3969e405a0e7575cda3dc03f52a9fd3533956f
Lusca04 <rodriguwalusca@gmail.com>
2026-04-21 21:41:14 -0300

    fix: Updating database to turn able multi actions

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,12 @@ import { connect } from "../utils/connection.js";
 export async function initDatabase() {
   const db = await connect();
 
+  await db.exec("PRAGMA journal_mode = WAL;");
+  await db.exec("PRAGMA synchronous = NORMAL;");
+  await db.exec("PRAGMA busy_timeout = 5000;");
+
+  await db.exec("PRAGMA temp_store = MEMORY;");
+
   await db.exec(`
     CREATE TABLE IF NOT EXISTS detections (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
