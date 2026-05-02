66e2a9189ef0db15c078e6656e7e524fb0af4683
guandrade09 <gustavoandradsousa00@gmail.com>
2026-05-02 12:24:59 -0300

    fix: subindo outro modelo treinado teste

--- Detecção-EPI---SPI-Challenge-5/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Detecção EPI - SPI Challenge > 2026-05-01 7:27pm
-https://universe.roboflow.com/andras-s-workspace/deteccao-epi-spi-challenge
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- ml_service/teste.py
+++ ml_service/teste.py
@@ -1,2 +1,8 @@
-import torch
-print(torch.cuda.is_available())
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="GvkijPJsitqTVsBp6bkX")
+project = rf.workspace().project("deteccao-epi-spi-challenge")
+version = project.version(7)
+
+# Baixa o modelo treinado
+version.download("yolov11")
