8abda143198e6b3f492703ad47a8bfb20a9bf16a
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-05-06 01:52:57 -0600

    chore: adicionando dependencia 'cors'

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import detectionRoutes from "./routes/detection.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api", detectionRoutes);
 app.use("/api", authRoutes);
