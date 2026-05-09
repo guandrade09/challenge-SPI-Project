000b12ee6dee2bdc7375b595663eecf79a4d10d1
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-05-09 00:34:43 -0600

    backend: adicionando ajustando cors e dependencias

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -7,7 +7,12 @@ import { ErrorHandler } from "./utils/appError.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "http://localhost:3300"],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  exposedHeaders: ['Content-Disposition', 'Content-Length']
+}));
+
 app.use(express.json({ limit: "50mb" }));
 app.use("/api", detectionRoutes);
 app.use("/api", authRoutes);
