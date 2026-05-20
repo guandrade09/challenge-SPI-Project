76804747cad9abd45fd84064fd2b126dbcb54b11
Lusca04 <rodriguwalusca@gmail.com>
2026-05-20 18:27:54 -0300

    feat: adicionando endpoint que retorna todos os relatorios em lista

--- backend/src/api/routes/report.routes.js
+++ backend/src/api/routes/report.routes.js
@@ -1,7 +1,7 @@
 // backend/src/api/routes/report.routes.js
 
 import { Router } from "express";
-import { downloadReportPdf, getReportPdf, downloadReportExcel, getReportSummary } from "../controllers/report.controller.js";
+import { downloadReportPdf, getReportPdf, downloadReportExcel, getReportSummary, getReportFiles } from "../controllers/report.controller.js";
 
 const router = Router();
 
@@ -9,7 +9,7 @@ router.get("/report/pdf", getReportPdf);
 router.get("/report/pdf/download", downloadReportPdf);
 router.get("/report/excel/download", downloadReportExcel);
 router.get("/report/pdf/summary", getReportSummary);
-
+router.get("/report/files", getReportFiles);
 
 
 export default router;
