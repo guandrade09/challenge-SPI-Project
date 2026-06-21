a42111e92e65ff469669ccebc3e77c3b124b59cf
Lacerdex <gabriel.gamexx1@hotmail.com>
2026-06-21 19:57:50 -0300

    refactor: ajustando rota front com back

--- backend/src/api/models/thread.model.js
+++ backend/src/api/models/thread.model.js
@@ -1,6 +1,6 @@
 export default class threadsConsume {
   constructor({ timestamp, thread_name, quantity_of_cpu_ind_percentage }) {
-    this.timestamp = Date.now().toISOString();
+    this.timestamp = new Date.now().toISOString();
     this.thread_name = thread_name;
     this.quantity_of_cpu_ind_percentage = quantity_of_cpu_ind_percentage;
   }
